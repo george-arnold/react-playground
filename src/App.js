a488497a66456ff650fce56fffc7d7522518da5a
George Arnold <george.arnold.88@gmail.com>
2019-12-26 13:15:53 -0500

    completed activities and drills

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Messages from './Messages';
+import TheDate from './state/TheDate';
+import Counter from './state/Counter'
 
 class App extends Component {
   render() {
@@ -9,6 +11,8 @@ class App extends Component {
         <h1>YOUR APPLICATION NAME!</h1>
         <Messages name="Messages" unread={0}/>
         <Messages name="Notifications" unread={10}/>
+        <TheDate />
+        <Counter count={123} />
       </div>
     );
   }
